a95b2d542b8269ffd7685fa14f654f496e6f8e6d
xcheko51x <xcheko51x@gmail.com>
2019-01-26 14:18:07 -0600

    Add files via upload

---
+++ CrearLeerXML.php
@@ -0,0 +1,51 @@
+<?php
+
+//CrearXML();
+LeerXML();
+
+function LeerXML() {
+	$usuarios = simplexml_load_file("/Users/xcheko51x/Downloads/usuarios.xml");
+
+	foreach ($usuarios as $usuario) {
+		echo "ID: " . $usuario->ID . "<br>";
+		echo "Nombre: " . $usuario->NOMBRE . "<br>";
+		echo "Telefono: " . $usuario->TELEFONO . "<br>";
+		echo "Email: " . $usuario->EMAIL . "<br>";
+	}
+}
+
+function CrearXML() {
+	$doc = new DOMDocument('1.0');
+
+	$doc->formatOutput = true;
+
+	$raiz = $doc->createElement("USUARIOS");
+	$raiz = $doc->appendChild($raiz);
+
+	$usuario = $doc->createElement("USUARIO");
+	$usuario = $raiz->appendChild($usuario);
+
+	$id = $doc->createElement("ID");
+	$id = $usuario->appendChild($id);
+	$textId = $doc->createTextNode(1);
+	$textId = $id->appendChild($textId);
+
+	$nombre = $doc->createElement("NOMBRE");
+	$nombre = $usuario->appendChild($nombre);
+	$textNombre = $doc->createTextNode("Sergio");
+	$textNombre = $nombre->appendChild($textNombre);
+
+	$telefono = $doc->createElement("TELEFONO");
+	$telefono = $usuario->appendChild($telefono);
+	$textTelefono = $doc->createTextNode("4324245432");
+	$textTelefono = $telefono->appendChild($textTelefono);
+
+	$email = $doc->createElement("EMAIL");
+	$email = $usuario->appendChild($email);
+	$textEmail = $doc->createTextNode("sergio@hola.es");
+	$textEmail = $email->appendChild($textEmail);
+
+	echo 'Escrito: ' . $doc->save("/Users/xcheko51x/Downloads/usuarios.xml") . 'bytes <br><br>';
+}
+
+?>
